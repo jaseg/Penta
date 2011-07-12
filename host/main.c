b060c62c584441d7624cdf78b253da25b6a71e26
Jaseg <s@jaseg.de>
2011-07-13 00:43:51 +0200

    Finished the test code. Now speed and acceleration should be calculated. Will possibly change the output format to generate valid csv data. After some test runs I will try to find a nice model for the deceleration. The µC would honestly be a nice place to contain the final code so the host app gets a "start/stop on corner 3" interface presented additionally to the low-level functions.

--- host/main.c
+++ host/main.c
@@ -16,6 +16,7 @@
 */
 
 #include <unistd.h>
+#include <sys/time.h>
 
 #include "opendevice.h"
 #include "../requests.h" //TODO relative path: not so nice.
@@ -29,7 +30,10 @@ int main(int argc, char** argv)
 		printf("Cannot open device handle (error code %d).\n", ec);
 		return 2;
 	}
-	int state = 1;
+	int state = 0;
+	unsigned long int last_position = 0UL;
+	unsigned long int start_time = 0UL;
+	double last_speed = 0.0;
 	while(1){
 		int transmitted = 0;
 		char data[8];
@@ -39,15 +43,44 @@ int main(int argc, char** argv)
 		}
 		//printf("Received interrupt data: %x\n", data[0]);
 		if(data[0] == PENTA_INT_BUTTON_PRESSED){
-			printf("Button pressed.\n");
+			//printf("Button pressed.\n");
+			state++;
+			state &= 0x0001;
+			if(state){
+				//(presumably) starting the motor
+				last_position = 0UL;
+				last_speed = 0.0;
+				struct timeval start_time_tv;
+				gettimeofday(&start_time_tv, 0);
+				start_time = start_time_tv.tv_sec*1000000UL+start_time_tv.tv_usec;
+				printf("Started at %lu\n", start_time);
+				if((ec = usb_control_msg(penta, 0x40, PENTA_REQ_SET_MOTOR, state, 0, 0, 0, 1000))){
+					printf("Problems with a motor control transfer (error code %d).\n", ec);
+					return 1;
+				}
+			}
+		}else{
+			//printf("Position encoder triggered\n");
+			struct timeval new_position_tv;
+			gettimeofday(&new_position_tv, 0);
+			unsigned long int new_position = new_position_tv.tv_sec*1000000UL+new_position_tv.tv_usec;
+			if(last_position != 0){
+				double speed = 1.0/(new_position-last_position);
+				printf("Speed at %lu: %f\n", new_position, speed);
+				if(last_speed != 0.0){
+					double accel = (speed-last_speed)/(new_position-last_position);
+					double mass_factor = 1.0/accel;
+					printf("\tAcceleration: %f\n", accel);
+					printf("\tMass factor: %f\n", mass_factor);
+				}
+				last_speed = speed;
+			}
+			last_position = new_position;
+			//Kludge ahead.
 			if((ec = usb_control_msg(penta, 0x40, PENTA_REQ_SET_MOTOR, state, 0, 0, 0, 1000))){
 				printf("Problems with a motor control transfer (error code %d).\n", ec);
 				return 1;
 			}
-			state++;
-			state &= 0x0001;
-		}else{
-			printf("Position encoder triggered\n");
 		}
 		//sleep(3);
 		/*if((ec = usb_control_msg(penta, 0x40, PENTA_REQ_SET_MOTOR, 0, 0, 0, 0, 1000))){
